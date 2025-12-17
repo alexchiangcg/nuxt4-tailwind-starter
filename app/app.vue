ff0d0a4ff486a3ae93cf66e1c59fb87d0fca8987
alex chiang <a25012@athena.com.tw>
2025-12-17 14:15:58 +0800

    feat: 新增 Syncfusion 註冊授權至 app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { appName } from '~/constants'
+import { registerLicense } from '@syncfusion/ej2-base'
+
+// 30.1.37 (Nov 7, 2025)
+registerLicense('Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWH5ednZUR2lfV0Z1VkNWYE0=')
+
 
 useHead({
   title: appName,
