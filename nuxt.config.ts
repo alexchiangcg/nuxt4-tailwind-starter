161354fa546a334a7fa41c5a0d8585358e7d01e9
alexChiang <a25012@athena.com.tw>
2025-12-24 11:49:25 +0800

    feat: 新增 devServer 設定以指定埠號

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,10 @@ export default defineNuxtConfig({
     enabled: true,
   },
 
+  devServer: {
+    port: 4567,
+  },
+
   app: {
     head: {
       viewport: 'width=device-width,initial-scale=1',
